5ac935355acd79afafd859e35ec6dd3f522fb770
Adwaith Nair <adwaith1310@gmail.com>
2024-01-03 13:01:23 +0530

    updated buy now btn

--- frontend/src/Components/BuyNow.jsx
+++ frontend/src/Components/BuyNow.jsx
@@ -3,12 +3,19 @@ import { useNavigate } from "react-router-dom";
 
 export default function BuyNow() {
   const navigate = useNavigate();
+  const loginToken = localStorage.getItem("loginToken");
+  const registerToken = localStorage.getItem("registerToken");
+
+  const thanksNavigate = () => {
+    if (!loginToken || !registerToken) {
+      navigate("/login");
+    } else {
+      navigate("/thanks");
+    }
+  };
   return (
     <div>
-      <button
-        className={cartbtnStyles.buybtn}
-        onClick={() => navigate("/thanks")}
-      >
+      <button className={cartbtnStyles.buybtn} onClick={thanksNavigate}>
         Buy Now
       </button>
     </div>
